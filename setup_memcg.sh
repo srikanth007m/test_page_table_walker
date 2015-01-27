29efb42c64d74fe3347bc50bc1c6eee0dbd50e42
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2015-01-27 16:47:37 +0900

    update memcg_move_task

--- setup_memcg.sh
+++ setup_memcg.sh
@@ -5,9 +5,16 @@ if [[ "$0" =~ "$BASH_SOURCE" ]] ; then
     exit 1
 fi
 
+if [ ! -d /sys/fs/cgroup/memory ] ; then
+    echo "memory cgroup is not supported on this kernel $(uname -r)"
+    return
+fi
+
 MEMCGDIR=/sys/fs/cgroup/memory
 MALLOC_MADV_WILLNEED=$(dirname $(readlink -f $BASH_SOURCE))/malloc_madv_willneed
 [ ! -x "$MALLOC_MADV_WILLNEED" ] && echo "${MALLOC_MADV_WILLNEED} not found." >&2 && exit 1
+
+yum install -y libcgroup-tools
 
 prepare_test() {
     get_kernel_message_before
@@ -15,8 +22,8 @@ prepare_test() {
 
 prepare_memcg() {
     pkill -9 sleep
-    cgdelete memory:test1
-    cgdelete memory:test2
+    cgdelete memory:test1 2> /dev/null
+    cgdelete memory:test2 2> /dev/null
     cgcreate -g memory:test1
     cgcreate -g memory:test2
     echo 1 > $MEMCGDIR/test1/memory.move_charge_at_immigrate
@@ -38,14 +45,16 @@ cleanup_memcg() {
 
 control_memcg() {
     cgexec -g memory:test1 sleep 1000 &
-    cgexec -g memory:test1 sleep 1000 &
-    cgexec -g memory:test1 sleep 1000 &
-    cgexec -g memory:test1 sleep 1000 &
-    cgexec -g memory:test1 sleep 1000 &
-    cat $MEMCGDIR/test1/tasks > $MEMCGDIR/test2/tasks || set_return_code MOVE_FAIL
-    cat $MEMCGDIR/test2/tasks > $MEMCGDIR/test1/tasks || set_return_code MOVE_FAIL
-    cat $MEMCGDIR/test1/tasks > $MEMCGDIR/test2/tasks || set_return_code MOVE_FAIL
-    cat $MEMCGDIR/test2/tasks > $MEMCGDIR/test1/tasks || set_return_code MOVE_FAIL
+    # cgexec -g memory:test1 sleep 1000 &
+    # cgexec -g memory:test1 sleep 1000 &
+    # cgexec -g memory:test1 sleep 1000 &
+    # cgexec -g memory:test1 sleep 1000 &
+    sleep 0.5
+    cat $MEMCGDIR/test1/tasks > $TMPF.test1_tasks_1
+    cat $MEMCGDIR/test2/tasks > $TMPF.test2_tasks_1
+    cgclassify -g memory:test1 $(cat $MEMCGDIR/test1/tasks) || set_return_code MOVE_FAIL
+    cat $MEMCGDIR/test1/tasks > $TMPF.test1_tasks_2
+    cat $MEMCGDIR/test2/tasks > $TMPF.test2_tasks_2
     pkill -9 sleep
     set_return_code "EXIT"
     return 0
@@ -59,6 +68,12 @@ check_test() {
 
 check_memcg() {
     check_test
+    count_testcount
+    if diff $TMPF.test1_tasks_1 $TMPF.test2_tasks_2 2> /dev/null >&2 ; then
+        count_success "processes moved from memory:test1 to memory:test2"
+    else
+        count_failure "processes failed to move from memory:test1 to memory:test2"
+    fi
 }
 
 prepare_force_swapin_readahead() {
@@ -79,15 +94,19 @@ cleanup_force_swapin_readahead() {
 
 control_force_swapin_readahead() {
     echo 0x1000000 > $MEMCGDIR/test1/memory.limit_in_bytes
-    cgset -r memory.limit_in_bytes=0x1000000 test1
-    cgexec -g memory:test1 $MALLOC_MADV_WILLNEED 0x1000000 &
+    echo 0x2000000 > $MEMCGDIR/test1/memory.memsw.limit_in_bytes
+    # cgset -r memory.limit_in_bytes=0x1000000 test1
+    # cgset -r memory.memsw.limit_in_bytes=0x2000000 test1
+    cgget -g memory:test1 | grep limit
+    cgexec -g memory:test1 $MALLOC_MADV_WILLNEED 0x1200000 &
     local pid=$!
-    echo $pid
+    # echo $pid
+    ps eo cmd,pid,pmem,rss $pid
     sleep 2
     free
     swapon -s
     cgset -r memory.limit_in_bytes=0x8000000 test1
-    kill -SIGUSR1 $pid
+    kill -SIGUSR1 $pid # MALLOC_MADV_WILLNEED calls madvise(MADV_WILLNEED)
     sleep 3
     free
     swapon -s
